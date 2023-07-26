dbdad4f33310526ec41ff9f3e55e570e51eb3870
Mika <mika_p_v@hotmail.com>
2023-07-26 11:40:28 +0300

    journal add page

--- src/main/java/mikan/tabulariusminimusmaximus/guielements/JournalAdd.java
+++ src/main/java/mikan/tabulariusminimusmaximus/guielements/JournalAdd.java
@@ -1,6 +1,11 @@
 package mikan.tabulariusminimusmaximus.guielements;
 
+import java.time.LocalDate;
+import java.time.chrono.Chronology;
+import java.util.Locale;
 import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -20,12 +25,26 @@ public class JournalAdd {
         journalView.setVgap(5);
         
         Label tositeIDLabel = new Label("Tositenumero");
-        Label tositeIDText = new Label(Integer.toString(nextEntryID));
+        Label tositepvmLabel = new Label("Päivämäärä");
+        Label seliteLabel = new Label("Selite");
+        Label tiliLabel = new Label("Tili");
         
-        Label test = new Label("koekenttä1");
-        TextField testField = new TextField();
+        Label tositeIDText = new Label(Integer.toString(nextEntryID));
+        DatePicker datePicker = new DatePicker();
+        TextField seliteTextField = new TextField();
+        ComboBox tiliCmbBox = new ComboBox();
+        
         journalView.add(tositeIDLabel, 0, 0);
+        journalView.add(tositepvmLabel, 0, 1);
+        journalView.add(seliteLabel, 0, 2);
+        journalView.add(tiliLabel, 0, 3);
+        
         journalView.add(tositeIDText, 1, 0);
+        journalView.add(datePicker, 1, 1);
+        journalView.add(seliteTextField, 1, 2);
+        journalView.add(tiliCmbBox, 1, 3);
+        
+        
         return journalView;
     }
 }
