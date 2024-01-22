40bf1008a87cd6566176381c9e2906a425717cad
Mika <mika_p_v@hotmail.com>
2024-01-22 05:24:45 +0200

    countHash JournalRow

--- src/test/java/mikan/tabulariusminimusmaximus/HashChecksumTest.java
+++ src/test/java/mikan/tabulariusminimusmaximus/HashChecksumTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package mikan.tabulariusminimusmaximus;
 
 import java.util.ArrayList;
@@ -32,7 +28,7 @@ public class HashChecksumTest {
      * Test of countHash method, of class HashChecksum.
      */
     @Test
-    public void testCountHash() throws Exception {
+    public void testCountHash_journal() throws Exception {
         
         String testPreviousHash = "8368f5f06e1b362211890e7717c274ce";
         ArrayList<JournalRow> testArray = new ArrayList<JournalRow>();
@@ -47,8 +43,20 @@ public class HashChecksumTest {
         HashChecksum checksum = new HashChecksum(testJournal, testPreviousHash);
         String result = checksum.countHash();
         String expectation = "6B319A7502B19E0D484A0A8DB1989D24";
+
+        assertEquals(expectation, result);
+    }
+    
+    @Test
+    public void testCountHash_jounalRow() throws Exception{
+    
+        String testPreviousHash = "8368f5f06e1b362211890e7717c274ce";
+        JournalRow journalRow = new JournalRow(777,11,22,99999,"testitili","");
+        HashChecksum checksum = new HashChecksum(journalRow, testPreviousHash);
+        String result = checksum.countHash();
+        String expectation = "7245F0E00604D4DA108852647E6A9934";
  
-        assertTrue(expectation.equals(result));
+        assertEquals(expectation, result);
     }
     
 }
